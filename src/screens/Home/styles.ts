7dbc71b15cc60c5283fa6254a0bbd60a1669381e
08igor <igordanielclarindo09@gmail.com>
2024-11-29 12:12:37 -0300

    Concluído

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    marginTop: 28,
+    
     flex: 1,
     backgroundColor: '#11181E',
     padding: 35,
